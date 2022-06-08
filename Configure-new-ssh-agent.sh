4701a33dfb843500dc6e63a4e7956e5d3ae67c5e
unman <unman@thirdeyesecurity.org>
2022-06-08 16:05:21 +0000

    Helper - fix

--- Configure-new-ssh-agent.sh
+++ Configure-new-ssh-agent.sh
@@ -1,31 +1,25 @@
-#!/bin/bash
+#!/usr/bin/bash
 echo "Set up a new ssh-agent y/n?" 
-read -n 1 response ;
+read -n1 -s response ;
 if [ "$response" == "y" ]; then
   read -p "What one word name do you want to use?" name ;
   if [ "$name" == "" ]; then
-    echo ""
     echo "No name entered. Nothing to do."
     exit
   else
-cat << EOF > /home/user/.local/share/systemd/user/ssh-$name.service:
-[Unit]
-Description=SSH agent for $name
-Before=default.target
-
-[Service]
-Type=forking
-Environment=SSH_AUTH_SOCK=%t/ssh-$name.socket
-ExecStart=/usr/bin/ssh-agent -a $SSH_AUTH_SOCK
-
-[Install]
-WantedBy=default.target
-EOF
-
+    if [ -f /home/user/.local/share/systemd/user/ssh-$name.service ] ; then
+      echo "That agent already exists"
+      exit
+    else
+      sed s/work/$name/  /home/user/.local/share/systemd/user/ssh-work.service > /home/user/.local/share/systemd/user/ssh-$name.service
+      ln -s /home/user/.local/share/systemd/user/ssh-$name.service /home/user/.config/systemd/user/default.target.wants/ssh-$name.service
+      head -n2 /home/user/work-agent.sh |sed s/work/$name/ > $name-agent.sh
+      chmod +x /home/user/$name-agent.sh
+      systemctl --user start ssh-$name.service
+    fi
   fi
 else
   clear
   echo "Nothing to do."
   exit
 fi
-
